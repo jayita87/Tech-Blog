0aec05b11e00431ea5a7ba69920edf3348f4665a
Jayita Chowdhury <jayitachowdhury@Amits-MacBook-Pro.local>
2022-10-24 16:43:50 -0700

    adding routes

--- Develop/controller/homeRoutes.js
+++ Develop/controller/homeRoutes.js
@@ -1,6 +1,110 @@
 const router = require('express').Router();
-router.get('/' ,(req,res) =>{
-    res.render('homepage')
-}
-)
+
+const { Post, User, Comment } = require('./models');
+const withAuth = require('./utils/auth');
+
+// Get all posts for homepage
+router.get('/', async (req, res) => {
+  try {
+    const postData = await Post.findAll({
+      include: [{ model: User }],
+    });
+    const posts = postData.map((post) => post.get({ plain: true }));
+
+    res.render('homepage', {
+      posts,
+      logged_in: req.session.logged_in
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+// Return page for a new post
+router.get('/post', withAuth, async (req, res) => {
+  res.render('create-post');
+});
+
+// Get a single post
+router.get('/post/:id', withAuth, async (req, res) => {
+  try {
+    const postData = await Post.findByPk(req.params.id, {
+      attributes: [
+        'id',
+        'title',
+        'post_body',
+        'date_created',
+      ],
+      include: [
+        {
+          model: User,
+          attributes: [
+            'username',
+          ],
+        },
+        {
+          model: Comment,
+          include: [
+            {
+              model: User,
+              attributes: [
+                'username',
+              ],
+            },
+          ],
+        },
+      ],
+    });
+
+    const post = postData.get({ plain: true });
+    
+    res.render('single-post', { 
+      post,
+      logged_in: req.session.logged_in 
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.get('/dashboard', withAuth, async (req, res) => {
+  try {
+    const postData = await Post.findAll({
+      where: {
+        user_id: req.session.user_id
+      },
+    })
+
+    const posts = postData.map((post) => post.get({ plain: true }));
+
+    res.render('dashboard', {
+      posts,
+      logged_in: req.session.logged_in
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+// Get route to login
+router.get('/login', (req, res) => {
+  // If the user is already logged in, redirect the request to another route
+  if (req.session.logged_in) {
+    res.redirect('/dashboard');
+    return;
+  }
+
+  res.render('login');
+});
+
+// Get route to signup
+router.get('/signup', (req, res) => {
+  if (req.session.logged_in) {
+    res.redirect('/');
+    return;
+  }
+
+  res.render('signup');
+});
+
 module.exports = router;
--- Develop/controller/index.js
+++ Develop/controller/index.js
@@ -1,10 +1,12 @@
 const router = require('express').Router();
 
-// const apiRoutes = require('./api');
+const apiRoutes = require('./api');
 const homeRoutes = require('./homeRoutes');
+const dashboardRoutes = require('./dashboardRoutes');
+
 
 router.use('/', homeRoutes);
-// router.use('/api', apiRoutes);
-
+router.use('/', dashboardRoutes);
+router.use('/api', apiRoutes);
 
 module.exports = router;
